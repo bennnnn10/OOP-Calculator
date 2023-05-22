e7a3c56b4ec29d549fad5cec60617730812805a3
unknown <rv8828120@gmail.com>
2023-05-22 22:28:51 +0800

    Division

--- Class_Calculator.py
+++ Class_Calculator.py
@@ -13,3 +13,5 @@ class Calculator:
     def multiplication(self, first_number, second_number):
         return first_number * second_number
     
+    def division(self, first_number, second_number):
+        return first_number / second_number
