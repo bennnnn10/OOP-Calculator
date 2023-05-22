059bbe10508d71ff0203ca0fcf30872f4041b339
unknown <rv8828120@gmail.com>
2023-05-22 22:54:24 +0800

    First and second number

--- User_Interface.py
+++ User_Interface.py
@@ -5,4 +5,12 @@ class Interface:
         print("Addition(1), Subtraction(2), Multiplication(3), or Division(4)")
         print("Choose one of the four math operations (1-4): ")
         math_operation = int(input(""))
+        return math_operation
+
+    def choosing_first_number(self):
+        first_number = float(input("Enter the first number: "))
+        return first_number
     
+    def choosing_second_number(self):
+        second_number = float(input("Enter the second number: "))
+        return second_number
