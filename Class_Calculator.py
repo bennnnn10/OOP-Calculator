2efd64b92a7f089bca8fe7c720a2dc07480194a8
unknown <rv8828120@gmail.com>
2023-05-22 22:26:48 +0800

    Multiplication

--- Class_Calculator.py
+++ Class_Calculator.py
@@ -10,3 +10,6 @@ class Calculator:
     def subtraction(self, first_number, second_number):
         return first_number - second_number
     
+    def multiplication(self, first_number, second_number):
+        return first_number * second_number
+    
