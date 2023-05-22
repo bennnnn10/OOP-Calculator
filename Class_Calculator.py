c04eeca8f400230cfd19c0087f2cd1eeb3dd054e
unknown <rv8828120@gmail.com>
2023-05-22 22:24:29 +0800

    Subtraction

--- Class_Calculator.py
+++ Class_Calculator.py
@@ -6,3 +6,7 @@ class Calculator:
 
     def addition(self, first_number, second_number):
         return first_number + second_number
+    
+    def subtraction(self, first_number, second_number):
+        return first_number - second_number
+    
